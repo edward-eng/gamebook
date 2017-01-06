2736453945213e648fcfb2f1f5eafbffad28503d
Kevin Goltermann <kggoltermann@gmail.com>
2017-01-06 15:38:40 -0600

    Send friend request feature works

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,11 @@
 class FriendshipsController < ApplicationController
   def create
-    Friendship.create(params[:friends])
+    Friendship.create(params[:friends].permit!)
      redirect_to :controller => 'users', :action => 'show', id: session[:users]
   end
+
+  private
+    def friendship_params
+      params.require(:friendship).permit(:accepting_id, :requesting_id)
+    end
 end
