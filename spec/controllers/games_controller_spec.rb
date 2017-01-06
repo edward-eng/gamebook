fc1e170032582dc2c4ae19178f64be070123fb18
Chris Massengale <chris.massengale@verizon.net>
2017-01-05 19:29:48 -0600

    Tried to adjust GamesController Show Method to pass test on CircleCi (passes locally)

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -10,7 +10,10 @@ describe GamesController do
 
   describe "games controller show method" do
     it "responds with a status code 200" do
-      get(:show, { id: 1 })
+      #get(:show, { id: 1 })
+
+      get :show, params: { id: 1 }, flash: { notice: "This is a flash message" },
+  session: nil # Can safely be omitted.
       expect(response).to have_http_status 200
     end
   end
